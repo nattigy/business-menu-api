f82a1991ed82526ee8e3fe17e2024b7a42e3e9b5
nattigy <nathnaelakale@gmail.com>
2021-03-13 21:44:44 +0300

    removed unncecessary code from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-// import bodyParser from "body-parser";
-import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 
 import "./utils/db";
@@ -10,23 +8,6 @@ import schema from "./schema";
 dotenv.config();
 
 const app = express();
-
-// app.use(bodyParser.json());
-
-// app.use(cors({
-//   credentials: true,
-//   // origin: 'http://localhost:8000',
-// }));
-
-// app.use((req, res, next) => {
-// res.setHeader("Access-Control-Allow-Origin", "*");
-// res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-// res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
 
 const server = new ApolloServer({
   schema,
