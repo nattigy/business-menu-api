498f89e9b755623c893a1219483e32a755bf26a9
nattigy <nathnaelakale@gmail.com>
2021-04-09 17:10:31 +0300

    added category index to categoiry nmodel

--- src/models/category.js
+++ src/models/category.js
@@ -14,6 +14,10 @@ export const CategorySchema = new Schema(
     autocompleteTerm: {
       type: String,
     },
+    categoryIndex: {
+      type: [String],
+      index: true
+    }
   },
   {
     collection: "categories",
