83c82491d8ed0ea35af2af6282c5279fbf02e1b1
nattigy <nathnaelakale@gmail.com>
2021-04-11 11:31:45 +0300

    fixed error on business model

--- src/models/business.js
+++ src/models/business.js
@@ -56,7 +56,7 @@ export const BusinessSchema = new Schema(
     },
     subscription: {
       type: String,
-      enum: ["Free", "1_Feather", "2_Feather", "3_Feather", "4_Feather"],
+      enum: ["FREE", "FEATHER_1", "FEATHER_2", "FEATHER_3", "FEATHER_4"],
       default: "Free",
     },
     openHours: {
